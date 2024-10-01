c67b66de96e416126884db0958ed71af97f174a3
vitor1B <147104338+vitor1BBBB@users.noreply.github.com>
2024-10-01 14:43:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é index.html?',
+    'Criada pelo britânico Tim Berners-Lee, o acrônimo HTML significa HiperText Markup Language, traduzindo ao português: Linguagem de Marcação de Hipertexto.  O HTML é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website. Portanto, ele serve para dar significado e organizar as informações de uma página na web. Sem isso, o navegador não saberia exibir textos como elementos ou carregar imagens e outros conteúdos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve o style CSS?',
+    'CSS é uma linguagem baseada em regras. — Você define regras especificando grupos de estilo que devem ser aplicados para elementos particulares ou grupos de elementos na sua página web. Por exemplo, "Quero que o título principal, na minha página, seja mostrado como um texto grande e de cor vermelha.".'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "boa noite" em Inglês?',
+    'Boa noite em ingles é Goodnight.'
+)
+criaCartao(
+    'Lingua Espanhol',
+    'Como "pedir licença" em Espanhol?',
+    'Pedir licença em Espanhol é "con permiso".'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é taxa Selic?',
+    'A taxa Selic é a taxa básica de juros da economia, que influencia outras taxas de juros do país, como taxas de empréstimos, financiamentos e aplicações financeiras. A definição da taxa Selic é o principal instrumento de política monetária utilizado pelo Banco Central (BC) para controlar a inflação..'
+)
